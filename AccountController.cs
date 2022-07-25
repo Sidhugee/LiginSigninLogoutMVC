e3d01772effb5f3d60659d4aad437c028fe045f1
Sallal Haider <87648696+Sidhugee@users.noreply.github.com>
2022-07-25 11:06:44 +0500

    Add files via upload

---
+++ AccountController.cs
@@ -0,0 +1,55 @@
+﻿using SignUpLoginLogoutApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+namespace SignUpLoginLogoutApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var Context = new Entities())
+            {
+                bool IsValid = Context.Users.Any(x => x.UserName == model.UserName && x.password == model.password);
+                if (IsValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "employees");
+                }
+                ModelState.AddModelError("", "Invalid username and password");
+                return View();
+            }
+           
+        }
+
+        // GET: Account
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var Context = new Entities())
+            {
+                Context.Users.Add(model);
+                Context.SaveChanges();
+            }
+            return RedirectToAction("login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
